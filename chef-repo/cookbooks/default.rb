efa3e622f5d7041995430484eb25e5f2739f16e3
Ricardo Gonzalez <rgonzalez@nisum.com>
2015-03-11 11:07:07 -0300

    default.rb: onmibus updates for chef version 11.64.4

--- chef-repo/cookbooks/default.rb
+++ chef-repo/cookbooks/default.rb
@@ -1,17 +1,20 @@
-include_recipe 'tomcat'
+include_recipe 'omnibus_updater'
+node.default['omnibus_updater']['version'] = '11.16.4'
 
-directory "#{node['tomcat']['webapp_dir']}/ROOT"
-
-service 'tomcat7' do
-  supports status: true, restart: true
-  action [:enable, :start]
-end
-
-file "#{node['tomcat']['webapp_dir']}/ROOT/index.html" do
-  action :create
-  owner 'root'
-  group 'root'
-  mode '0644'
-  content 'Hello from tomcat :)'
-  notifies :restart, 'service[tomcat7]'
-end
+#include_recipe 'tomcat'
+#
+#directory "#{node['tomcat']['webapp_dir']}/ROOT"
+#
+#service 'tomcat7' do
+#  supports status: true, restart: true
+#  action [:enable, :start]
+# end
+#
+# file "#{node['tomcat']['webapp_dir']}/ROOT/index.html" do
+#   action :create
+#   owner 'root'
+#   group 'root'
+#   mode '0644'
+#   content 'Hello from tomcat :)'
+#   notifies :restart, 'service[tomcat7]'
+# end
