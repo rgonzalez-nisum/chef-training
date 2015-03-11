90f9d35327209eb511b226822fa86ff763c1dab8
Ricardo Gonzalez <rgonzalez@nisum.com>
2015-03-11 18:50:47 -0300

    bla bla bla

--- chef-repo/cookbooks/onBoarding/metadata.rb
+++ chef-repo/cookbooks/onBoarding/metadata.rb
@@ -8,4 +8,3 @@ version          '0.1.0'
 
 depends "java"
 depends "tomcat7"
-depends "git"
--- chef-repo/cookbooks/onBoarding/recipes/default.rb
+++ chef-repo/cookbooks/onBoarding/recipes/default.rb
@@ -12,4 +12,3 @@ node.override['java']['oracle']['accept_oracle_download_terms'] = true
 
 include_recipe "java"
 include_recipe "tomcat7"
-include_recipe "git"
