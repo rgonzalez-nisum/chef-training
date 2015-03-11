7ff0518e99724e05c54295e80ba216b15190cd3b
Ricardo Gonzalez <rgonzalez@nisum.com>
2015-03-11 19:05:18 -0300

    Restart

--- chef-repo/cookbooks/onBoarding/recipes/default.rb
+++ chef-repo/cookbooks/onBoarding/recipes/default.rb
@@ -6,9 +6,3 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-node.override['java']['jdk_version'] = '7'
-node.override['java']['install_flavor'] = "oracle"
-node.override['java']['oracle']['accept_oracle_download_terms'] = true
-
-include_recipe "java"
-include_recipe "tomcat7"
