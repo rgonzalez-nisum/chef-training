7904d6bd6d2809c946a7c86024c3dee9653be205
Ricardo Gonzalez <rgonzalez@nisum.com>
2015-03-12 10:35:58 -0300

    Restart again

--- chef-repo/cookbooks/onBoarding/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'onBoarding'
-maintainer       'Ricardo Gonzalez'
-maintainer_email 'rgonzalez@nisum.com'
-license          'All rights reserved'
-description      'Installs/Configures onBoarding'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
